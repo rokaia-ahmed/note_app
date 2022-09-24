5b18741f91acc60edb021374b7ebb95932631a64
salma <salmaseif_20@yahoo.com>
2022-09-24 20:59:32 +0200

    mainScreen1

--- lib/views/widgets/listViewItem.dart
+++ lib/views/widgets/listViewItem.dart
@@ -5,42 +5,45 @@ class listViewItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-        return   Container(
-          // margin: EdgeInsets.all(10),
-          padding: EdgeInsets.all(10),
-          height: 150,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              color: Colors.blue
+        return   Dismissible(
+          background:buildSwipeActionRight() ,
+          key: Key("ih"),
+          child: Container(
+            // margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            height: 150,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.blue
 
-          ),
-          child:
+            ),
+            child:
 
 
 
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
-            children: [
+              children: [
 
-              Row(
-                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Note Title",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
-                  Spacer(),
-                  IconButton(onPressed: (){}, icon: Icon(Icons.favorite)),
+                Row(
+                  //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Note Title",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+                    Spacer(),
+                    IconButton(onPressed: (){}, icon: Icon(Icons.favorite)),
 
-                  IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
-                ],
-              ),
-              Expanded(
-                child: Text(" bhjbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-                  textAlign: TextAlign.start,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 9,
+                    IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
+                  ],
                 ),
-              ),
+                Expanded(
+                  child: Text(" bhjbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+                    textAlign: TextAlign.start,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 9,
+                  ),
+                ),
 
 
 
@@ -49,10 +52,17 @@ class listViewItem extends StatelessWidget {
 
 
 
-            ],
+              ],
+            ),
+
           ),
-
         );
 
   }
+  Widget buildSwipeActionRight() => Container(
+    alignment: Alignment.centerRight,
+    padding: EdgeInsets.symmetric(horizontal: 20),
+    color: Colors.red,
+    child: Icon(Icons.delete_forever, color: Colors.white, size: 32),
+  );
 }
