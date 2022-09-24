2ec9da12d6b4076194768e09ad927431e772cfb5
Rokaia Ahmed <rokiaahmed42@gmail.com>
2022-09-25 00:38:01 +0200

    refactor1

--- lib/shared/app_colors.dart
+++ lib/shared/app_colors.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
 const primaryColor = Color(0xff303030);
 const lightGreen  = Color(0xff00bfa5);
-<<<<<<<<< Temporary merge branch 1
-//11118888
-=========
-//t0000
->>>>>>>>> Temporary merge branch 2
+
