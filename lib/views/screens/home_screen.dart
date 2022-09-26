e7947f55f2c5fc38cc1cbdd8fdaa9da2a468ce95
SuhaylaYounis <60473969+SuhaylaYounis@users.noreply.github.com>
2022-09-26 15:45:27 +0200

    editnotescreen

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -17,117 +17,112 @@ class Home_screen extends StatefulWidget {
 
 class _Home_screenState extends State<Home_screen> {
   List<NoteModel>? allNotes;
-  bool isListView= true;
- @override
+  bool isListView = true;
+
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
     BlocProvider.of<NotesCubit>(context).getAllTask();
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          backgroundColor: Colors.black,
-          appBar: AppBar(backgroundColor: Colors.transparent,
-            title: Text("My Notes"),
-            actions: [
-              IconButton(icon:Icon(Icons.favorite,color: Colors.red,),
-                onPressed: () {
-                },
-              ),
-              IconButton(
-                  onPressed: (){
-                    setState(() {
-                      isListView=!isListView;
-                    });
-                    /*Navigator.push(context,MaterialPageRoute(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text("My Notes"),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.red,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  isListView = !isListView;
+                });
+                /*Navigator.push(context,MaterialPageRoute(
                         builder: (context)=>GridViewItem()
                     ),
                     );*/
-                  },
-                  icon: Icon(Icons.grid_view)
-              ),
-
-            ],
-          ),
-          body: buildBody(),
-          floatingActionButton: FloatingActionButton(
-            backgroundColor:lightGreen ,
-            child:Icon(Icons.add_outlined) ,
-            onPressed:(){
-              print('tuyrtytwe');
-              Navigator.push(context,MaterialPageRoute(
-                builder: (context)=>AddNote(),
-              ),
-              );
-            } ,
-          ),
-        );
+              },
+              icon: Icon(Icons.grid_view)),
+        ],
+      ),
+      body: buildBody(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: lightGreen,
+        child: Icon(Icons.add_outlined),
+        onPressed: () {
+          print('tuyrtytwe');
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddNote(),
+            ),
+          );
+        },
+      ),
+    );
   }
 
   Widget buildBody() {
-    return BlocBuilder<NotesCubit,NotesStates>(
-      builder: (context, state) {
-
-         if(state is NotesgetDbLoadingState)
-         {
-           allNotes=(state).allNotes;
-           print('notew $allNotes');
-           return  Padding(
-             padding: const EdgeInsets.all(15),
-             child: Column(
-             crossAxisAlignment: CrossAxisAlignment.end,
-             children: [
-               Text("swipe left to delete",style: TextStyle(color: Colors.red,fontSize: 10),),
-               SizedBox(height: 15,),
-               isListView ?Expanded(
-
-                 child: ListView.builder(
-
-                  shrinkWrap: true,
-                  itemCount: allNotes!.length,
-                  itemBuilder: (BuildContext context,int index){ return listViewItem(note: allNotes![index],);},
-                 //separatorBuilder: (BuildContext context, int index) { return SizedBox(height: 20,); }, itemCount: 5),
-
-               ))
-
-               :Expanded(
-                   child: GridView.builder(
-                   gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
-                       crossAxisCount: 2,
-                       childAspectRatio: 1,
-                       crossAxisSpacing: 0.5,
-                       mainAxisSpacing: 0.5,
-                   ),
-                       shrinkWrap: true,
-                     physics: ClampingScrollPhysics(),
-                     padding: EdgeInsets.zero,
-                     itemCount:allNotes!.length,
-                     itemBuilder: (BuildContext context, int index)
-                     {
-                       return listViewItem(note: allNotes![index]);
-                     },
-                   ))
-
-
-             ],
-             ),
-             );
-         }
-         else{
-
-           return ShowloadingIndicator();
-         }
-
-
-
-
-
-
-      } );
-
+    return BlocBuilder<NotesCubit, NotesStates>(builder: (context, state) {
+      if (state is NotesgetDbLoadingState) {
+        allNotes = (state).allNotes;
+        print('notew $allNotes');
+        return Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                "swipe left to delete",
+                style: TextStyle(color: Colors.red, fontSize: 10),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              isListView
+                  ? Expanded(
+                      child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: allNotes!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return listViewItem(
+                          note: allNotes![index],
+                        );
+                      },
+                      //separatorBuilder: (BuildContext context, int index) { return SizedBox(height: 20,); }, itemCount: 5),
+                    ))
+                  : Expanded(
+                      child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 1,
+                        crossAxisSpacing: 0.5,
+                        mainAxisSpacing: 0.5,
+                      ),
+                      shrinkWrap: true,
+                      physics: ClampingScrollPhysics(),
+                      padding: EdgeInsets.zero,
+                      itemCount: allNotes!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return listViewItem(note: allNotes![index]);
+                      },
+                    ))
+            ],
+          ),
+        );
+      } else {
+        return ShowloadingIndicator();
+      }
+    });
   }
 }
-
