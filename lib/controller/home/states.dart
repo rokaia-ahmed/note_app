6af59b246a28b8db7bd6b8d1a9b977a3e11dca79
Rayane <krachnirayan17@gmail.com>
2022-09-26 01:17:34 -0700

    Merge branch 'main' into rayan

--- lib/controller/home/states.dart
+++ lib/controller/home/states.dart
@@ -1,9 +1,22 @@
+import 'package:notes/models/notes_model.dart';
+
 abstract class NotesStates {}
 
 class NotesInitialState extends NotesStates {}
 class NotesCreateDbState extends NotesStates {}
-class NotesgetDbLoadingState extends NotesStates {}
+class NotesgetDbLoadingState extends NotesStates {
+  final List<NoteModel> allNotes;
+  NotesgetDbLoadingState(this.allNotes);
+
+}
+class NotesgetFavNotesState extends NotesStates {
+  final List<NoteModel> favNotes;
+  NotesgetFavNotesState(this.favNotes);
+
+}
 class NotesgetDbState extends NotesStates {}
 class NotesUpdateDbState extends NotesStates {}
-class NotesInsertDbState extends NotesStates {}
+class NotesInsertDbState extends NotesStates {
+
+}
 class NotesDeleteDbState extends NotesStates {}
--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -3,8 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes/controller/home/cubit.dart';
 import 'package:notes/controller/home/states.dart';
 import 'package:notes/models/db_config.dart';
+import 'package:notes/models/notes_model.dart';
 import 'package:notes/shared/app_colors.dart';
 import 'package:notes/views/widgets/listViewItem.dart';
+import 'package:notes/views/widgets/show_indicator.dart';
 import '../widgets/GridviewItem.dart';
 import 'add_note.dart';
 
@@ -14,20 +16,19 @@ class Home_screen extends StatefulWidget {
 }
 
 class _Home_screenState extends State<Home_screen> {
- // @override
- /* void initState() {
+  List<NoteModel>? allNotes;
+  bool isListView= true;
+ @override
+  void initState() {
     // TODO: implement initState
     super.initState();
-    SqlDb database = new SqlDb();
-    database.initialDb();
-  }*/
+    BlocProvider.of<NotesCubit>(context).getAllTask();
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<NotesCubit,NotesStates>(
-      listener:(context,state){} ,
-      builder:(context,state){
-        return Scaffold(
+    return Scaffold(
           backgroundColor: Colors.black,
           appBar: AppBar(backgroundColor: Colors.transparent,
             title: Text("My Notes"),
@@ -38,31 +39,20 @@ class _Home_screenState extends State<Home_screen> {
               ),
               IconButton(
                   onPressed: (){
-                    Navigator.push(context,MaterialPageRoute(
+                    setState(() {
+                      isListView=!isListView;
+                    });
+                    /*Navigator.push(context,MaterialPageRoute(
                         builder: (context)=>GridViewItem()
                     ),
-                    );
+                    );*/
                   },
                   icon: Icon(Icons.grid_view)
               ),
 
             ],
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text("swipe left to delete",style: TextStyle(color: Colors.red,fontSize: 10),),
-                SizedBox(height: 15,),
-                Expanded(child: ListView.separated(
-                    shrinkWrap: true,
-                    itemBuilder: (BuildContext context,int index){ return listViewItem();},
-                    separatorBuilder: (BuildContext context, int index) { return SizedBox(height: 20,); }, itemCount: 5),)
-
-              ],
-            ),
-          ),
+          body: buildBody(),
           floatingActionButton: FloatingActionButton(
             backgroundColor:lightGreen ,
             child:Icon(Icons.add_outlined) ,
@@ -75,7 +65,69 @@ class _Home_screenState extends State<Home_screen> {
             } ,
           ),
         );
-      } ,
-    );
+  }
+
+  Widget buildBody() {
+    return BlocBuilder<NotesCubit,NotesStates>(
+      builder: (context, state) {
+
+         if(state is NotesgetDbLoadingState)
+         {
+           allNotes=(state).allNotes;
+           print('notew $allNotes');
+           return  Padding(
+             padding: const EdgeInsets.all(15),
+             child: Column(
+             crossAxisAlignment: CrossAxisAlignment.end,
+             children: [
+               Text("swipe left to delete",style: TextStyle(color: Colors.red,fontSize: 10),),
+               SizedBox(height: 15,),
+               isListView ?Expanded(
+
+                 child: ListView.builder(
+
+                  shrinkWrap: true,
+                  itemCount: allNotes!.length,
+                  itemBuilder: (BuildContext context,int index){ return listViewItem(note: allNotes![index],);},
+                 //separatorBuilder: (BuildContext context, int index) { return SizedBox(height: 20,); }, itemCount: 5),
+
+               ))
+
+               :Expanded(
+                   child: GridView.builder(
+                   gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+                       crossAxisCount: 2,
+                       childAspectRatio: 1,
+                       crossAxisSpacing: 0.5,
+                       mainAxisSpacing: 0.5,
+                   ),
+                       shrinkWrap: true,
+                     physics: ClampingScrollPhysics(),
+                     padding: EdgeInsets.zero,
+                     itemCount:allNotes!.length,
+                     itemBuilder: (BuildContext context, int index)
+                     {
+                       return listViewItem(note: allNotes![index]);
+                     },
+                   ))
+
+
+             ],
+             ),
+             );
+         }
+         else{
+
+           return ShowloadingIndicator();
+         }
+
+
+
+
+
+
+      } );
+
   }
 }
+
